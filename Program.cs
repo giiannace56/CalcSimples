ed1690d00c9f8e65a67d5cd37751ba6ff5bf205b
giovanni iannace <giovanniiannace56@gmail.com>
2020-11-16 14:36:12 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace calculadora_simples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual operação quer realiziar \nSoma , Subtracao, Multiplicacao , Divisao");
+            string operacao = Console.ReadLine();
+
+            Console.WriteLine("digite o 1 numero");
+            float num1 =float.Parse(Console.ReadLine());
+
+            Console.WriteLine("digite o 2 numero");
+            float num2 =float.Parse(Console.ReadLine());
+
+            float resultado = 0f;
+
+            
+
+        }
+    }
+}
