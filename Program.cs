3ecb853348c6145d2d4f26085dcbac8430fe5195
giovanni iannace <giovanniiannace56@gmail.com>
2020-11-16 15:42:54 -0300

    calculadora concluida

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace calculadora_simples
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Qual operação quer realiziar \nSoma , Subtracao, Multiplicacao , Divisao");
+            Console.WriteLine("Qual operação quer realiziar /nSoma , Subtracao, Multiplicacao , Divisao");
             string operacao = Console.ReadLine();
 
             Console.WriteLine("digite o 1 numero");
@@ -17,7 +17,31 @@ namespace calculadora_simples
 
             float resultado = 0f;
 
-            
+            switch(operacao){
+                case "Soma" :
+                resultado = num1 + num2 ;
+                break;
+
+                case "Subtracao" :
+                resultado = num1 - num2 ;
+                break;
+
+                case "Multiplicacao" :
+                resultado = num1 * num2 ;
+                break;
+
+                case "Divisao" :
+                resultado = num1 / num2 ;
+                break;
+
+                default:
+                    Console.WriteLine("Operação inválida");
+                    break;
+            }
+
+                Console.WriteLine($"Calculo : {num1} com {num2} = {resultado}");
+
+
 
         }
     }
